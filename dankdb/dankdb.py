4d1f20ca5c723e9f8e8625ba61ed30f62440b474
Hunter Hopper <hunterlhopper@gmail.com>
2020-07-29 17:33:24 -0500

    Added CSV functionality and added newlines after print statements

--- dankdb/dankdb.py
+++ dankdb/dankdb.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 
 
 class Database():
@@ -14,7 +15,10 @@ class Database():
 
 		self.table_name = ""
 
-		print("Database initialized...")
+		self.values = []
+		self.columns = []
+
+		print("Database initialized...\n")
 
 	def create_table(self, table_name, *args, **kwargs):
 		"""This function will take a table name and mutliple column names then create a table"""
@@ -34,7 +38,7 @@ class Database():
 		command = f"CREATE TABLE {self.table_name} {string}"
 		self.c.execute(command)
 		self.conn.commit()
-		print("Table created...")
+		print("Table created...\n")
 
 
 	def insert_row(self, *args, **kwargs):
@@ -51,24 +55,45 @@ class Database():
 		command = f"INSERT INTO {self.table_name} VALUES {string}"
 		self.c.execute(command, args)
 		self.conn.commit()
-		print("Values added...")
+		print("Values added...\n")
 
 	def delete_rows(self):
 		"""This function will remove all rows from db"""
 		command = f"DELETE FROM {self.table_name}"
 		self.c.execute(command)
 		self.conn.commit()
-		print("All rows removed...")
+		print("All rows removed...\n")
 
 	def fetch_table(self):
 		"""This function will retrieve all values from the table"""
 		self.c.execute("SELECT * FROM " + self.table_name)
 		values = self.c.fetchall()
 		self.conn.commit()
-		print("Values retrieved...")
+		print("Values retrieved...\n")
 		return values
+
+	def export_csv(self, file_name):
+		"""This function will export all data in db"""
+		self.c.execute("SELECT * FROM "+ self.table_name)
+		columns = [tuple[0] for tuple in self.c.description]
+		rows = self.fetch_table()
+
+		with open(f'{file_name}.csv', 'w') as f:
+		    writer = csv.writer(f, delimiter='\t')
+		    writer.writerow(columns)
+		    for row in rows:
+		    	writer.writerow(row)
+		print("DB exported to csv...\n")
+
+	def read_csv(self, file_name):
+		"""This function will print out whats in the csv"""
+		with open(f'{file_name}.csv', 'r') as f:
+			reader = csv.reader(f, delimiter="\t")
+			for row in reader:
+				if (row):
+					print(row)
 
 	def close(self):
 		"""This function will close the connection to the db"""
 		self.conn.close()
-		print("Connection to db closed...")
+		print("Connection to db closed...\n")
